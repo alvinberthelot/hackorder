cf98dfe5facc6ed79842e6119a26c74482d76705
alvinberthelot <alvin.berthelot@gmail.com>
2014-05-09 15:29:56 +0200

    feta(refonte massive): Pfiou bis ...

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,39 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+
+    // compass: {                  // Task
+    //   dist: {                   // Target
+    //     options: {              // Target options
+    //       sassDir: 'scss',
+    //       cssDir: 'css',
+    //       force: true,
+    //       require: 'zurb-foundation',
+    //       environment: 'production'
+    //     }
+    //   },
+    //   dev: {                    // Another target
+    //     options: {
+    //       sassDir: 'scss',
+    //       cssDir: 'css',
+    //       force: true,
+    //       require: 'zurb-foundation'
+    //     }
+    //   }
+    // },
+
+
     sass: {
       options: {
         includePaths: ['bower_components/foundation/scss']
+      },
+      dev:{
+        options: {
+          outputStyle: 'expanded'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }
       },
       dist: {
         options: {
@@ -18,17 +48,21 @@ module.exports = function(grunt) {
 
     watch: {
       grunt: { files: ['Gruntfile.js'] },
-
+      // compass: {
+      //   files: ['/styles/{,*/}*.{scss,sass}'],
+      //   tasks: ['compass']
+      // },
       sass: {
         files: 'scss/**/*.scss',
-        tasks: ['sass']
+        tasks: ['sass:dev']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  // grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('build', ['sass:dist']);
+  grunt.registerTask('default', ['sass:dev','watch']);
 }
